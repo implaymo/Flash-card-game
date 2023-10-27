abba74c0080a3859af5176eea82ef09d08adf1bc
implaymo <goncalomorerasilva@gmail.com>
2023-10-27 16:54:07 +0100

    Changed changed_word() to get_french_word() so it gives me a french word everytime i use this function

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ BACKGROUND_COLOR = "#B1DDC6"
 random_word = ""
 
 
-def change_word():
+def get_french_word():
     global random_word
     # Opens CSV as a dataframe and gets it into a list of dictionaries
     csv_dataframe = pandas.read_csv("data/french_words.csv")
-    words_dict = csv_dataframe.to_dict('records')
+    data_dict = csv_dataframe.to_dict('records')
     words_list = []
     # Gets each value of dictionary to words_list
-    for dictionary in words_dict:
-        for key,value in dictionary.items():
-            words_list.append(value)
+    for dictionary in data_dict:
+        words_list.append(dictionary["French"])
     canvas.delete("text")
     random_word = random.choice(words_list)
+
     # Word text
     canvas.create_text(400, 263, text=random_word, fill="black", font=("Arial", 60, "bold"), tags="text")
+    # Language Type
+    canvas.create_text(400, 150, text="French", fill="black", font=("Arial", 40, "italic"))
 
 
 # Setup window
@@ -38,21 +40,18 @@ canvas.create_image(400, 263, image=front_image)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Starting word
-change_word()
+get_french_word()
 
 # Create WRONG button
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=change_word)
+wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=get_french_word)
 wrong_button.grid(column=0,row=1)
 
 
 # Create RIGHT button
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=change_word)
+right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=get_french_word)
 right_button.grid(column=1, row=1)
-
-# Language Type
-canvas.create_text(400, 150, text="French", fill="black", font=("Arial", 40, "italic"))
 
 
 
