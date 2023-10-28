c5a23ff2786da7aeb96cf50bc30ce41a6be8be44
implaymo <goncalomorerasilva@gmail.com>
2023-10-28 21:47:53 +0100

    Added english_card() function and made my card flip to the back card.

--- main.py
+++ main.py
@@ -11,17 +11,19 @@ data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient='records')
 
 
-def next_card():
-    random_french_word = random.choice(to_learn)
-    canvas.itemconfig(text_word, text=random_french_word["French"])
+def french_card():
+    random_word = random.choice(to_learn)
+    canvas.itemconfig(text_word, text=random_word["French"])
     canvas.itemconfig(text_title, text="French")
+    canvas.itemconfig(canvas_image, image=front_image)
+    window.after(3000)
 
-
-# def flip_card():
-#     if wrong_button or right_button and Word French
-#         canvas.itemconfig(GET ENGLISH WORD)
-#     if wrong_button or right_button and Word English
-#         canvas.itemconfig(GET FRENCH WORD)
+def english_card():
+    canvas.itemconfig(canvas_image, image=back_image)
+    random_word = random.choice(to_learn)
+    canvas.itemconfig(text_word, text=random_word["English"])
+    canvas.itemconfig(text_title, text="English")
+    window.after(3000)
 
 # Setup window
 window = Tk()
@@ -32,7 +34,8 @@ window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # Create canvas
 canvas = Canvas(width=800, height=526)
 front_image = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=front_image)
+back_image = PhotoImage(file="./images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=front_image)
 text_word = canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
 text_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
 canvas.config(bg= BACKGROUND_COLOR, highlightthickness=0)
@@ -41,16 +44,15 @@ canvas.grid(column=0, row=0, columnspan=2)
 
 # Create WRONG button
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=next_card)
+wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=french_card)
 wrong_button.grid(column=0,row=1)
 
 
 # Create RIGHT button
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=next_card)
+right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=english_card)
 right_button.grid(column=1, row=1)
 
-next_card()
-
+french_card()
 
 window.mainloop()
